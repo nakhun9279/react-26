68a666d75721d895231ac629edbfaf0481eb36ac
nakhun9279 <fjfj9279@naver.com>
2020-09-07 18:23:32 +0900

    24.2.1 UI준비하기

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 
 const RegisterPage = () => {
-  return <div>회원가입</div>;
+  return (
+    <AuthTemplate>
+      <AuthForm />
+    </AuthTemplate>
+  );
 };
 
 
 
 export default RegisterPage;
-
