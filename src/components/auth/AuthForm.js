d46168abb9d6da60ba138d6969cae9a9597a9024
nakhun9279 <fjfj9279@naver.com>
2020-09-07 20:06:20 +0900

    24.2.2 리덕스로 폼 상태 관리하기

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -59,25 +59,35 @@ const textMap = {
 };
 
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
   const text = textMap[type];
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
-      <form>
-        <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+      <form onSubmit={onSubmit}>
+        <StyledInput
+          autoComplete="username"
+          name="username"
+          placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
+        />
         <StyledInput
           autoComplete="new-password"
           name="password"
           placeholder="비밀번호"
           type="password"
+          onChange={onChange}
+          value={form.password}
         />
         {type === 'register' && (
           <StyledInput
-            autoComplete="new-password" 
+            autoComplete="new-password"
             name="passwordConfirm"
             placeholder="비밀번호 확인"
             type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
           />
         )}
         <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
@@ -94,7 +104,6 @@ const AuthForm = ({ type }) => {
     </AuthFormBlock>
   );
 };
-
 
 
 export default AuthForm;
