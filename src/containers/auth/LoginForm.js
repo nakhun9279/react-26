758fea0886e85a43c5c4c666700d053be2a28e2a
nakhun9279 <fjfj9279@naver.com>
2020-09-08 02:31:00 +0900

    24.2.6 회원 인증 에러 처리하기

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { changeField, initializeForm, login } from '../../modules/auth';
@@ -6,6 +6,7 @@ import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
@@ -45,6 +46,7 @@ useEffect(() => {
   if (authError) {
     console.log('오류 발생');
     console.log(authError);
+    setError('로그인 실패')
     return;
   }
   if (auth) {
@@ -67,6 +69,7 @@ return (
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
