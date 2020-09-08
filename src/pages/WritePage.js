d6f169191f311aa09e3177884c3c55288d5385b1
nakhun9279 <fjfj9279@naver.com>
2020-09-08 22:01:33 +0900

    25.2.1 TAGBox만들기

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 import Responsive from '../components/common/Responsive';
-
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
-
-
 
 export default WritePage;
