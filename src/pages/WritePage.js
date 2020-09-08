3fa3d87957fbc10e918086f4ff0bc2b7554dbdc2
nakhun9279 <fjfj9279@naver.com>
2020-09-08 21:52:56 +0900

    25.1 에디터 UI구현하기

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Editor from '../components/write/Editor';
+import Responsive from '../components/common/Responsive';
 
 
 const WritePage = () => {
-  return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      <Editor />
+    </Responsive>
+  );
 };
 
 
