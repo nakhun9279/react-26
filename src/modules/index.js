4a57b550d7c69e2643fcabbc6fb1cbf589ea481b
nakhun9279 <fjfj9279@naver.com>
2020-09-08 22:36:48 +0900

    25.3.3 글쓰기 api 연동하기

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,7 +3,8 @@ import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
-import write from './write';
+import write, { writeSaga } from './write';
+
 
 const rootReducer = combineReducers({
   auth,
@@ -12,8 +13,12 @@ const rootReducer = combineReducers({
   write,
 });
 
+
+
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga()]);
 }
+
+
 
 export default rootReducer;
