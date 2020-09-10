461dd278ad5420724d0fc3d9ea2ab83fba745c88
nakhun9279 <fjfj9279@naver.com>
2020-09-11 03:01:30 +0900

    27.2 포스트 삭제

--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -5,22 +5,22 @@ import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 import PostActionButtons from '../../components/post/PostActionButtons';
 import { setOriginalPost } from '../../modules/write';
-
+import { removePost } from '../../lib/api/posts';
 
 const PostViewerContainer = ({ match, history }) => {
   // 처음 마운트될 때 포스트 읽기 API 요청
   const { postId } = match.params;
   const dispatch = useDispatch();
-  const { post, error, loading, user } = useSelector(({ post, loading, user }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-    user: user.user,
-  }));
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
 
-
-
-useEffect(() => {
+  useEffect(() => {
     dispatch(readPost(postId));
     // 언마운트될 때 리덕스에서 포스트 데이터 없애기
     return () => {
@@ -28,26 +28,32 @@ useEffect(() => {
     };
   }, [dispatch, postId]);
 
-
-
-const onEdit = () => {
+  const onEdit = () => {
     dispatch(setOriginalPost(post));
     history.push('/write');
   };
 
+  const onRemove = async () => {
+    try {
+      await removePost(postId);
+      history.push('/'); // 홈으로 이동
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
+  const ownPost = (user && user._id) === (post && post.user._id);
 
-return (
+  return (
     <PostViewer
       post={post}
       loading={loading}
       error={error}
-      actionButtons={<PostActionButtons onEdit={onEdit} />}
-      ownPost={user && user.id === post && post.id}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+      }
     />
   );
 };
-
-
 
 export default withRouter(PostViewerContainer);
